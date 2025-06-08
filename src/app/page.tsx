69ce8a531d5fe43b9a0f1a7f455dc7b949020a1b
Ekansh Sharma <70947204+ekansh28@users.noreply.github.com>
2025-06-08 18:21:33 +0530

    some bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -342,7 +342,7 @@ export default function SelectionLobby() {
             )}
           >
             <Image
-              src="/icons/discord.png"
+              src="/icons/discord.gif"
               alt="discord"
               width={isMobile ? 32 : 40}
               height={isMobile ? 32 : 40}
