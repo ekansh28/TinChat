cf2d7121712a4fb3677cb9eab6e3b5cf8ad42909
Ekansh Sharma <70947204+ekansh28@users.noreply.github.com>
2025-06-13 06:36:06 +0530

    added one more win 7 theme

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -26,6 +26,7 @@ const available7Stamps: ThemeStamp[] = [
   { name: 'Frutiger Aero', imageUrl: '/theme_stamps/frutiger.png', cssFile: 'frutiger1-theme.css', dataAiHint: 'frutiger theme stamp' },
   { name: 'Frutiger Aero 2', imageUrl: '/theme_stamps/frutiger2.png', cssFile: 'frutiger2-theme.css', dataAiHint: 'frutiger2 theme stamp' },
   { name: 'Frutiger Aero 3', imageUrl: '/theme_stamps/frutiger3.png', cssFile: 'frutiger3-theme.css', dataAiHint: 'frutiger3 theme stamp' },
+  { name: 'idk', imageUrl: '/theme_stamps/beautiful.png', cssFile: 'beautiful-theme.css', dataAiHint: 'beautiful theme stamp' },
   { name: 'Vector Bloom', imageUrl: '/theme_stamps/vectorbloom.png', cssFile: 'vectorbloom-theme.css', dataAiHint: 'vectorbloom theme stamp' },
   { name: 'Vector Bloom 2', imageUrl: '/theme_stamps/vectorbloom2.png', cssFile: 'vectorbloom2-theme.css', dataAiHint: 'vectorbloom2 theme stamp' },
   { name: 'Vector Bloom 3', imageUrl: '/theme_stamps/vectorbloom3.png', cssFile: 'vectorbloom3-theme.css', dataAiHint: 'vectorbloom3 theme stamp' },
