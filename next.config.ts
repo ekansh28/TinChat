19c1594e9185fc1979015267ebf81bec739bb908
Ekansh Sharma <70947204+ekansh28@users.noreply.github.com>
2025-06-21 06:17:04 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,18 +12,23 @@ const nextConfig = {
 
     const cspHeader = `
       default-src 'self' ${cdn};
-      script-src 'self' 'unsafe-eval' 'unsafe-inline' ${cdn} *.paypal.com *.paypalobjects.com *.googleapis.com https://unpkg.com;
+      script-src 'self' 'unsafe-eval' 'unsafe-inline' ${cdn} *.paypal.com *.paypalobjects.com *.googleapis.com https://unpkg.com https://static.cloudflareinsights.com;
+      script-src-elem 'self' 'unsafe-inline' ${cdn} *.paypal.com *.paypalobjects.com *.googleapis.com https://unpkg.com https://static.cloudflareinsights.com;
       style-src 'self' 'unsafe-inline' ${cdn} https://fonts.googleapis.com https://unpkg.com;
       font-src 'self' ${cdn} https://fonts.gstatic.com https://unpkg.com;
-      img-src 'self' data: blob: *;  # This allows images from any source
-      media-src 'self' blob: *;  # This allows media from any source
-      connect-src 'self' ${cdn} ${socketServerUrl} ${socketWsUrl} ${socketWssUrl} ws://localhost:3000 ws://127.0.0.1:3000 ws://localhost:3001 ws://127.0.0.1:3001 wss://localhost:3001 wss://127.0.0.1:3001 wss://${supabaseHostname} https://${supabaseHostname} https://unpkg.com https://s3-us-east-2.amazonaws.com;
+      img-src 'self' data: blob: *;
+      media-src 'self' blob: *;
+      connect-src 'self' ${cdn} ${socketServerUrl} ${socketWsUrl} ${socketWssUrl} 
+        ws://localhost:3000 ws://127.0.0.1:3000 ws://localhost:3001 ws://127.0.0.1:3001 
+        wss://localhost:3001 wss://127.0.0.1:3001 wss://${supabaseHostname} 
+        https://${supabaseHostname} https://unpkg.com https://s3-us-east-2.amazonaws.com 
+        https://static.cloudflareinsights.com;
       frame-src 'self' ${cdn} *.youtube.com *.paypal.com;
       object-src 'self' ${cdn};
       worker-src 'self' ${cdn} blob:;
       manifest-src 'self' ${cdn};
       base-uri 'self';
-    `.replace(/\s{2,}/g, " ").trim();
+    `.replace(/\n/g, ' ').trim();
 
     return [
       {
